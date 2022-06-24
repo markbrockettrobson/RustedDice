9b9f3a18b960eb279820360b200c5191e3b7eb65
markbrockettrobson <markbrockettrobson@gmail.com>
2022-06-24 22:27:57 +1000

    add mock tests

--- rusted_dice/src/my_mock_add.rs
+++ rusted_dice/src/my_mock_add.rs
@@ -5,16 +5,22 @@ pub fn mock_add(first : i32, second : i32) -> i32 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test_case::test_case;
+    use proptest::prelude::*;
 
-    #[test]
-    fn this_test_should_fail() {
-        let result = mock_add(2, 1);
-        assert_eq!(result, 4);
+    #[test_case(-2, -4, -6 ; "when both operands are negative")]
+    #[test_case(-4,  2, -2 ; "whem 1st operand is negative")]
+    #[test_case(2,  -4, -2 ; "when 2nd operand is negative")]
+    fn this_test_should_fail(operand_one: i32, operand_two: i32, expected_awnser: i32) {
+        let result = mock_add(operand_one, operand_two);
+        assert_eq!(result, expected_awnser);
     }
 
-    #[test]
-    fn this_test_should_pass() {
-        let result = mock_add(12, 92);
-        assert_eq!(result, 104);
+    proptest! {
+        #[test]
+        fn add_positive_should_be_larger(basevalue: i32, positive: u16) {
+            let result = mock_add(basevalue, positive.into());
+            assert!(result >= basevalue);
+        }
     }
 }
