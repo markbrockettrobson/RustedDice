ca2b26ed6f67938e72d9bd9a6d404182e25ef0b1
Mark Brockett Robson <47262668+markbrockettrobson@users.noreply.github.com>
2023-05-21 14:20:05 +0000

    add factory functions

--- rusted_dice/src/probability/probability_outcome/probability_outcome_factory.rs
+++ rusted_dice/src/probability/probability_outcome/probability_outcome_factory.rs
@@ -1,4 +1,6 @@
-use crate::constraint_management::{ConstraintMapFactory, ConstraintValueType};
+use crate::constraint_management::{
+    Constraint, ConstraintMap, ConstraintMapFactory, ConstraintValueType,
+};
 use crate::probability::ProbabilityOutcome;
 
 #[allow(dead_code)]
@@ -7,10 +9,29 @@ pub struct ProbabilityOutcomeFactory;
 #[allow(dead_code)]
 impl ProbabilityOutcomeFactory {
     pub(crate) fn new_with_empty_constraint_map(value: ConstraintValueType) -> ProbabilityOutcome {
-        let empty_constraint_map = ConstraintMapFactory::new_empty_constraint_map();
         ProbabilityOutcome {
             value,
-            constraint_map: empty_constraint_map,
+            constraint_map: ConstraintMapFactory::new_empty_constraint_map(),
+        }
+    }
+
+    pub(crate) fn new_with_constraint_map(
+        value: ConstraintValueType,
+        constraint_map: ConstraintMap,
+    ) -> ProbabilityOutcome {
+        ProbabilityOutcome {
+            value,
+            constraint_map,
+        }
+    }
+
+    pub(crate) fn new_with_constraints(
+        value: ConstraintValueType,
+        constraints: impl IntoIterator<Item = Constraint>,
+    ) -> ProbabilityOutcome {
+        ProbabilityOutcome {
+            value,
+            constraint_map: ConstraintMapFactory::new_constraint_map(constraints),
         }
     }
 }
@@ -18,6 +39,7 @@ impl ProbabilityOutcomeFactory {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::constraint_management::ConstraintFactory;
     use proptest::prelude::*;
 
     proptest! {
@@ -25,7 +47,38 @@ mod tests {
         fn test_new_empty_constraint(test_value: ConstraintValueType) {
             let probability_outcome = ProbabilityOutcomeFactory::new_with_empty_constraint_map(test_value);
             assert!(probability_outcome.value == test_value);
-            assert_eq!(probability_outcome.constraint_map.map.len(), 0);
+            assert_eq!(probability_outcome.constraint_map, ConstraintMapFactory::new_empty_constraint_map());
+        }
+
+        #[test]
+        fn test_new_with_constraint_map(test_value: ConstraintValueType) {
+            let test_constraint_map = ConstraintMapFactory::new_constraint_map(vec![
+                ConstraintFactory::new_many_item_constraint(2, vec![1, 2, 3]),
+                ConstraintFactory::new_many_item_constraint(3, vec![1, 2, 3]),
+            ]);
+            let probability_outcome = ProbabilityOutcomeFactory::new_with_constraint_map(test_value, test_constraint_map.clone());
+            assert!(probability_outcome.value == test_value);
+            assert_eq!(probability_outcome.constraint_map, test_constraint_map);
+        }
+
+        #[test]
+        fn test_new_with_constraints(test_value: ConstraintValueType) {
+            let test_constraints = vec![
+                ConstraintFactory::new_many_item_constraint(1, vec![1]),
+                ConstraintFactory::new_many_item_constraint(2, vec![1, 2]),
+                ConstraintFactory::new_many_item_constraint(3, vec![1, 2, 3]),
+                ConstraintFactory::new_many_item_constraint(4, vec![1, 2, 3, 4]),
+            ];
+            let probability_outcome = ProbabilityOutcomeFactory::new_with_constraints(test_value, test_constraints.clone());
+            assert!(probability_outcome.value == test_value);
+            assert_eq!(probability_outcome.constraint_map, ConstraintMapFactory::new_constraint_map(test_constraints));
+        }
+
+        #[test]
+        fn test_new_with_constraints_empty(test_value: ConstraintValueType) {
+            let probability_outcome = ProbabilityOutcomeFactory::new_with_constraints(test_value, vec![]);
+            assert!(probability_outcome.value == test_value);
+            assert_eq!(probability_outcome.constraint_map, ConstraintMapFactory::new_constraint_map(vec![]));
         }
     }
 }
