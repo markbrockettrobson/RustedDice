e26204ded9fa2a636fe42c9074db99c2ebd4e4cf
Mark Brockett Robson <47262668+markbrockettrobson@users.noreply.github.com>
2023-05-23 12:01:06 +0000

    use simple constructors

--- rusted_dice/src/probability/probability_distribution.rs
+++ rusted_dice/src/probability/probability_distribution.rs
@@ -10,20 +10,11 @@ pub struct ProbabilityDistribution {
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
-
-    use crate::constraint_management::constraint_map::ConstraintMap;
-
     use super::*;
 
     #[test]
     fn outcome_counts_set() {
-        let test_outcome = ProbabilityOutcome {
-            value: 12345,
-            constraint_map: ConstraintMap {
-                map: HashMap::new(),
-            },
-        };
+        let test_outcome = ProbabilityOutcome::new_with_empty_constraint_map(12345);
 
         let mut b_tree_map = BTreeMap::new();
         b_tree_map.insert(test_outcome.clone(), 67890);
@@ -35,24 +26,9 @@ mod tests {
     }
     #[test]
     fn test_fmt() {
-        let test_outcome_one = ProbabilityOutcome {
-            value: 1111,
-            constraint_map: ConstraintMap {
-                map: HashMap::new(),
-            },
-        };
-        let test_outcome_two = ProbabilityOutcome {
-            value: 2222,
-            constraint_map: ConstraintMap {
-                map: HashMap::new(),
-            },
-        };
-        let test_outcome_three = ProbabilityOutcome {
-            value: 3333,
-            constraint_map: ConstraintMap {
-                map: HashMap::new(),
-            },
-        };
+        let test_outcome_one = ProbabilityOutcome::new_with_empty_constraint_map(1111);
+        let test_outcome_two = ProbabilityOutcome::new_with_empty_constraint_map(2222);
+        let test_outcome_three = ProbabilityOutcome::new_with_empty_constraint_map(3333);
 
         let mut b_tree_map = BTreeMap::new();
         b_tree_map.insert(test_outcome_one.clone(), 1);
